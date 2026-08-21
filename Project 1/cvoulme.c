fec60757d9e2b38dd01becb843ce0f7008f377ee
Praphul4251M <prafulkumar942@gmail.com>
2026-08-21 14:41:31 +0530

    Volume calculation program for a cylinder

---
+++ Project 1/cvoulme.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int r = 4;
+    int h = 20;
+    printf("Volume of the cylinder with radius %d and height %d is %f", r, h , 3.14*r*r*h); // this is how respectively we can use int and float datatype in printf function
+    return 0;
+}
+// data cane taken as input from user using sacnf function
